c1f62ea406285f9f5f0ce9a57b7308c326fc97bf
naoki <n.globe.us@gmail.com>
2023-02-18 00:11:02 +0900

    some fix

--- src/Map.tsx
+++ src/Map.tsx
@@ -398,8 +398,8 @@ const Component = () => {
         </div>
       </div>
       <div className='absolute bottom-0 py-px px-9 text-[8px] text-left  bg-white sm:max-w-lg lg:max-w-none lg:right-[270px] lg:text-[12px] lg:px-2'>
-        <span><a className=' text-blue-600 hover:underline' href="https://front.geospatial.jp/houmu-chiseki/" target="_blank" rel="noreferrer">「登記所備付データ」（法務省）</a>を加工して作成（数値以外で始まる地番住所は除外しています）</span>
-        <span><a className='text-blue-600 hover:underline' href="https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v3_1.html" target="_blank" rel="noreferrer">「国土数値情報（行政区域データ）」（国土交通省）</a>を加工して作成</span>
+        <span><a className=' text-blue-600 hover:underline' href="https://front.geospatial.jp/houmu-chiseki/" target="_blank" rel="noreferrer">「登記所備付データ」（法務省）</a>を加工して作成。</span>
+        <span><a className='text-blue-600 hover:underline' href="https://github.com/geolonia/chiban-kokyozahyo-area/blob/main/README.md" target="_blank" rel="noreferrer">使用データ、計算方法についてはこちら</a></span>
       </div>
     </>
   );
